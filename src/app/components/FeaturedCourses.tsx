4a0822eb49bd71234fae95aa9405d9b2e6dbb5cb
akshay-sharma062 <akshay.ku.sharma.062@gmail.com>
2025-05-12 01:24:09 +0530

    this is nextjs project

---
+++ src/app/components/FeaturedCourses.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { BackgroundGradient } from "./ui/background-gradient";
+import coursedata from "../data/music_courses.json";
+import Link from "next/link";
+
+interface Course{
+  
+        id: number,
+        title: String,
+        slug: String,
+        description:String,
+        price: number,
+        instructor: String,
+        isFeatured: boolean,  
+}
+function FeaturedCourses() {
+    const featuredCourse=coursedata.courses.filter((course:Course) => course.isFeatured)
+
+  return (
+    <div className="py-12 bg-gray-900">
+      <div className="text-center">
+        <h2>Featured Courses</h2>
+        <p>Learn with the best</p>
+      </div>
+      <div className="mt-10">
+        <div className="grid grid-col-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center ">
+            {featuredCourse.map((course:Course)=>(
+              <div className="flex justify-center" key={course.id}>
+                <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                                <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
+                                <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
+                                <Link href={`/courses/${course.slug}`} className="mt-3 border-2 border-transparent hover:border-2 hover:border-cyan-300 rounded-2xl p-2 duration-700 hover:shadow-cyan-400 hover:shadow-2xl">
+                                Learn More
+                                </Link>
+                            </div>
+                </BackgroundGradient>
+              </div>
+            ))}
+        </div>
+
+      </div>
+      <div className="mt-20 text-center">
+        <Link href={"/courses"} className="p-4 text-xl border-2 border-cyan-300 hover:border-transparent hover:text-cyan-300 hover:border-2 hover:border-b-cyan-300 rounded-2xl  duration-700 hover:shadow-cyan-400 hover:shadow-2xl">View all Courses</Link>
+      </div>
+    </div>
+  );
+}
+
+export default FeaturedCourses;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 
+import FeaturedCourses from "./components/FeaturedCourses";
 import HeroSection from "./components/HeroSection";
 export default function Home() {
   return (
    <main className="bg-black/[0.96] min-h-screen antialiased bg-grid-white/[0.02]">
     <HeroSection />
+    <FeaturedCourses />
    </main>
   );
 }
